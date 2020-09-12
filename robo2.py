0d4e67f69e4dde9dcec27e469b8d1080059a979f
Jocelino Garcia <39247713+jocelinoFG017@users.noreply.github.com>
2020-09-12 18:52:43 -0300

    Update robo2.py

--- robo2.py
+++ robo2.py
@@ -8,12 +8,12 @@ destinatario = "jocelinogg@yandex.com"
 assunto = "enviando ymail por robo"
 mensagem = "A morte é triste, mas a vida é pior..."
 
-driver = webdriver.Firefox("")
+driver = webdriver.Firefox(r"C:\Users\JocelinoFG\Desktop\Robos")
 
 print("iniciando robo..")
 print("Acessando yandexmail..")
 
-url = ""
+url = "https://passport.yandex.com/auth/add?from=mail&origin=hostroot_homer_auth_L_com&retpath=https%3A%2F%2Fmail.yandex.com%2F%3Fuid%3D1170422679&backpath=https%3A%2F%2Fmail.yandex.com%3Fnoretpath%3D1"
 driver.get(url)
 
 print("Realizando login..")
@@ -28,16 +28,16 @@ time.sleep(5)
 password = driver.find_element_by_id("passp-field-passwd")
 password.send_keys(senha)
 password.send_keys(Keys.RETURN)
-time.sleep(5)
+time.sleep(7)
 
 #abrir caixa de ymail
-botao = driver.find_element_by_xpath("//div[@class='mail-ComposeButton-Wrap js-compose-button-container']")
+botao = driver.find_element_by_class_name("//div[@class='mail-ComposeButton-Wrap js-compose-button-container']")
 botao.click()
 time.sleep(5)
 
 #destinatario
 
-para  = driver.find_element_by_xpath("//div[@class='composeYabbles']")
+para = driver.find_element_by_xpath("//div[@class='composeYabbles']")
 para.send_keys(destinatario)
 para.send_keys(Keys.RETURN)
 time.sleep(5)
@@ -50,6 +50,7 @@ time.sleep(5)
 
 #mensagem
 
+
 letter = driver.find_element_by_xpath("//div[@class='cke_wysiwyg_div cke_reset cke_enable_context_menu cke_editable cke_editable_themed cke_contents_ltr cke_htmlplaceholder']")
 letter.send_keys(mensagem)
 time.sleep(5)
@@ -57,3 +58,8 @@ time.sleep(5)
 
 
 driver.close()
+
+
+#pagina util
+
+#http://pythonclub.com.br/selenium-parte-1.html
